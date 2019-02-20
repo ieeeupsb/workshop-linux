b9f65c52c74928dcbb2206410038bc248c4afb7b
Fábio Gaspar <fabiodanielreis@hotmail.com>
2019-02-20 17:46:24 +0000

    Setting up

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let slideshow = remark.create({
+	sourceUrl: 'main.md',
+
+	/* highlight */
+	highlightLanguage: 'c',
+	highlightStyle: 'zenburn',
+	highlightLines: 'true',
+	highlightSpans: 'false',
+});
